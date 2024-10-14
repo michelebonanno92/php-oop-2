1d7752aa0bc36f63fbc45c1f075bd57b4eae2eac
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-14 15:29:05 +0200

    creata classe product

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+    class Product {
+        
+        public $category ;
+        public $image ;
+        public $title ;
+        public $price ;
+        public $icon ;
+        public $type ;
+
+
+    }
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+            Ciao Gente
+        </h1>
+    </body>
+</html>
